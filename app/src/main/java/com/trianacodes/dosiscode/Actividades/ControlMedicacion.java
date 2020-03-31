be4eb84950931b26a097a366b4052a495299c5e3
Worome <trianacodes@gmail.com>
2020-03-31 13:37:48 +0200

    Initial commit y realizado lo siguiente:                     * Diseñados las actividades principal, de mensajes y de control de medicación.                     * creada clase que representa a los objetos medicamentos.                     * Desarrollo del códifo de lanzamiento de la actrividad de control de medicamento pasándole como parámetro un objeto de tipo medicamento.                     * Creado DatePicker.                     * Creado TimePicker.                     * Manejo de fechas.                     * Cálculo de Fechas y Horas en las que hay que tomar el medicamento

--- app/src/main/java/com/trianacodes/dosiscode/Actividades/ControlMedicacion.java
+++ app/src/main/java/com/trianacodes/dosiscode/Actividades/ControlMedicacion.java
@@ -23,7 +23,8 @@ import java.util.Locale;
 public class ControlMedicacion extends AppCompatActivity {
 
     private TextView MedicamentoControl,FechaControl, HoraControl, NumeroDiasControl,
-            NumeroHoraControl, TextoResultado;
+            NumeroHoraControl, TextoResultado, TextoResultado1;
+    private int totalHoras, totalTramos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class ControlMedicacion extends AppCompatActivity {
         NumeroDiasControl = findViewById(R.id.tvNumeroDias);
         NumeroHoraControl = findViewById(R.id.tvNumeroHoras);
         TextoResultado = findViewById(R.id.etResultado);
+        TextoResultado1 = findViewById(R.id.etHora);
 
         RecogeDatos();
         try {
@@ -61,23 +63,54 @@ public class ControlMedicacion extends AppCompatActivity {
     public void CalculaFechaFinal() throws ParseException {
 
         String fechaString = FechaControl.getText().toString();
+
         // Defino el formato de la fecha en forma de String
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
         // Le doy el formato al String de la fecha
         Date fecha = formato.parse(fechaString);
 
         //Creo una instancia de tipo Calendar para manejar la cadena de fecha
         Calendar FechaFin = Calendar.getInstance();
+
         // Doy el valor de la cadena de fecha al objeto de tipo Calendar
         FechaFin.setTime(fecha);
         //Sumo un número de días a la fecha
-        FechaFin.add(Calendar.DAY_OF_YEAR, Integer.parseInt(NumeroDiasControl.getText().toString()) - 1);
+        //FechaFin.add(Calendar.DAY_OF_YEAR, Integer.parseInt(NumeroDiasControl.getText().toString()) - 1);
+
         // Defino el formato de fecha según la región del móvil
         DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+
         // Asigno al objeto Date que he creado el valor la fecha resultante
         fecha = FechaFin.getTime();
+
         // Muestro el resultado asignándole el formato definido correspondiente a la región del móvil
-        TextoResultado.setText(df.format(FechaFin.getTime()));
-    }
+        //TextoResultado.setText(df.format(FechaFin.getTime()));
+
+        int HorasDia = 24 / Integer.parseInt(NumeroHoraControl.getText().toString());
+        int HoraFin, MinutoFin, Indice;
+        int SumatorioDia = 0;
+        // Veo en la posición en la que se encuentran los : dentro de la cadena de la hora
+        Indice = HoraControl.getText().toString().indexOf(":");
+        HoraFin = Integer.parseInt(HoraControl.getText().toString().substring(0,Indice));
+        MinutoFin = Integer.parseInt(HoraControl.getText().toString().substring(Indice + 1, 5));
+        Toast.makeText(this,df.format(FechaFin.getTime()) +  "@@@@" + HoraFin + ":" + MinutoFin, Toast.LENGTH_LONG).show();
+
+           for (int j = 0; j < (Integer.parseInt(NumeroDiasControl.getText().toString()) * HorasDia) - 1; j++){
+
+               HoraFin = HoraFin + Integer.parseInt(NumeroHoraControl.getText().toString());
+               if (HoraFin > 24){
+
+                   HoraFin = HoraFin - 24;
+                   SumatorioDia = 1;
+                   FechaFin.add(Calendar.DAY_OF_YEAR,SumatorioDia);
+
+               }
+
+               Toast.makeText(this,df.format(FechaFin.getTime()) +  "@@@@" + HoraFin + ":" + MinutoFin, Toast.LENGTH_LONG).show();
+
+            }
+
+        }
 
 }
